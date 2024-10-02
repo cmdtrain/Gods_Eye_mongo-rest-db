7ad8b2aed2ff7752a50a91e27dc7d9f13a280471
cmdtrain <72573030+cmdtrain@users.noreply.github.com>
2024-10-02 14:16:29 +0300

    build.gradle update

--- src/main/java/com/example/restservice/domain/Person.java
+++ src/main/java/com/example/restservice/domain/Person.java
@@ -14,28 +14,41 @@ public class Person {
 	private String firstName;
 	private String lastName;
 	private Long profession;
-	
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", profession=" + profession
+				+ "]";
+	}
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public Long getProfession() {
 		return profession;
 	}
+
 	public void setProfession(Long profession) {
 		this.profession = profession;
 	}
