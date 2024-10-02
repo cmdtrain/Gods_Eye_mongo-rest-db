6cf4e2b6add11510a98d0a416ed717efc7058195
cmdtrain <72573030+cmdtrain@users.noreply.github.com>
2024-10-02 16:29:26 +0300

    added exceptionvalidation

---
+++ src/main/java/com/example/restservice/service/PersonValidationException.java
@@ -0,0 +1,12 @@
+package com.example.restservice.service;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class PersonValidationException extends Exception { 
+    public PersonValidationException(String errorMessage) {
+        super(errorMessage);
+   
+
+}
+    }
