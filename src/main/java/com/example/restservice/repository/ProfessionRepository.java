cacdc068e5c471b419c9fb49d5c487b9c093ee46
cmdtrain <72573030+cmdtrain@users.noreply.github.com>
2024-10-04 11:49:39 +0300

    del profrepository

--- src/main/java/com/example/restservice/repository/ProfessionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.restservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-import com.example.restservice.domain.Person;
-
-@Repository(value = "profession")
-public interface ProfessionRepository extends MongoRepository<Person, String> {
-}
